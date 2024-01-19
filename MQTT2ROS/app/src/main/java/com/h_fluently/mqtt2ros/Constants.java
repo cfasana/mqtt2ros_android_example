78ce722c443f64d6e7449563451dcf42160c35ef
cfasana <corrado.fasana@mch-tronics.ch>
2024-01-19 10:58:00 +0100

    Added ROS2MQTT video

--- MQTT2ROS/app/src/main/java/com/h_fluently/mqtt2ros/Constants.java
+++ MQTT2ROS/app/src/main/java/com/h_fluently/mqtt2ros/Constants.java
@@ -1,13 +1,13 @@
 package com.h_fluently.mqtt2ros;
 
 public class Constants {
-    public static final String MQTT_BROKER_URI = "tcp://<HOST_IP>:1883";
+    public static final String MQTT_BROKER_URI = "tcp://10.170.10.13:1883";
     public static final String MQTT_USERNAME = "";
     public static final String MQTT_PSW = "";
     public static final int MQTT_QOS = 2;
     public static final boolean MQTT_RETAINED = false;
     public static final String MQTT_CLIENT_ID = "";
-    public static final String MQTT_SUBSCRIPTION_TOPIC = "<SUBSCRIBE_TOPIC>";
-    public static final String MQTT_PUBLISHED_TOPIC = "<PUBLISH_TOPIC>>";
+    public static final String MQTT_SUBSCRIPTION_TOPIC = "mqtt/ros2mqtt_msg";
+    public static final String MQTT_PUBLISHED_TOPIC = "mqtt/mqtt2ros_msg";
     public static final String MQTT_PUBLISHED_MSG = "This topic is sent from MQTT.";
 }
